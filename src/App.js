b053de8d1a01076efd9517e1f31a5ed243295c6a
Andrey Astafev <servertdot@gmail.com>
2023-05-30 10:53:58 +0300

    4444

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ function App() {
 
   const onClick1 = () => {
     // console.log('window.top.location.href', window.top.location.href);
-    window.top.location.href = '/iframe-redirect'
+    // window.top.location.href = '/iframe-redirect'
+    console.log(window.top)
+    console.log(window.parent)
 
   }
 
   const onClick2 = () => {
     // console.log('window.top.location.href', window.top.location.href);
-    window.parent.location.href = '/iframe-redirect'
+    // window.parent.location.href = '/iframe-redirect'
   }
 
   return (
